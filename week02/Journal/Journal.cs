b29afc7602ebaa849e4af3dac2463233285f0b99
bcastillo11 <byroncv2811@outlook.com>
2025-09-13 01:31:14 -0500

    Journal first attempt to build

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,109 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry currentEntry in _entries)
+        {
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(currentEntry._date);
+            Console.WriteLine(currentEntry._prompt);
+            Console.WriteLine(currentEntry._entryText);
+        }
+    }
+
+    public bool SaveToFile(string file)
+    {
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                foreach (Entry entry in _entries)
+                {
+                    sw.WriteLine($"DATE: {entry._date}");
+                    sw.WriteLine($"PROMPT: {entry._prompt}");
+                    sw.WriteLine($"ENTRY: {entry._entryText}");
+                    sw.WriteLine(new string('-', 30));
+                }
+            }
+
+            return true;
+        }
+        catch (FileNotFoundException)
+        {
+            return false;
+        }
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // Regex regex = new Regex(@"DATE:\s*(?<fecha>\d{4}/\d{2}/\d{2})\r?\nPROMPT:\s*(?<prompt>.+)\r?\nENTRY:\r?\n(?<entry>.*?)(?=\r?\n---|$)",
+            // RegexOptions.Singleline);
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string line;
+                Entry current = null;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.StartsWith("DATE:"))
+                    {
+                        if (current != null)
+                        {
+                            _entries.Add(current);
+                        }
+
+                        current = new Entry();
+                        current._date = line.Replace("DATE:", "").Trim();
+                    }
+                    else if (line.StartsWith("PROMPT:"))
+                    {
+                        current._prompt = line.Replace("PROMPT:", "").Trim();
+                    }
+                    else if (line.StartsWith("ENTRY:"))
+                    {
+                        string entryString = line.Replace("ENTRY:", "");
+
+                        while ((line = sr.ReadLine()) != null && !line.StartsWith("---"))
+                        {
+                            entryString = entryString + line + Environment.NewLine;
+                        }
+
+                        current._entryText = entryString.Trim();
+                    }
+                    else if (line.StartsWith("---"))
+                    {
+                        if (current != null)
+                        {
+                            _entries.Add(current);
+                            current = new Entry();
+                        }
+                    }
+                }
+
+                if (current != null)
+                {
+                    _entries.Add(current);
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found");
+        }
+        
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,97 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        // Console.WriteLine("Hello World! This is the Journal Project.");
+        PromptGenerator prompt = new PromptGenerator();
+        Journal myJournal = new Journal();
+        Entry newEntry = new Entry();
+
+        Console.WriteLine("Welcome to your Journal.");
+        Console.WriteLine("Enter the umber of what you would like to do:");
+
+        while (true)
+        {
+            Console.WriteLine($"1. New Entry{Environment.NewLine}2. Display Journal{Environment.NewLine}3. Save journal to a new file{Environment.NewLine}4. Load journal from a different file.{Environment.NewLine}5. Exit.");
+            Console.Write("Select: ");
+            string inputChoice = Console.ReadLine();
+            int choice;
+
+            while (!int.TryParse(inputChoice, out choice) || choice < 1 || choice > 5)
+            {
+                Console.Write($"'{inputChoice}': is not valid, please try again: ");
+                inputChoice = Console.ReadLine();
+            }
+
+            if (choice == 1)
+            {
+                string currentPrompt = prompt.GetRandomPrompt();
+                Console.WriteLine(currentPrompt);
+                string inputEntry = Console.ReadLine();
+
+                while (inputEntry == null)
+                {
+                    Console.WriteLine("Empty entries not allowed. Try again:");
+                    Console.Write("Your entry: ");
+                    inputEntry = Console.ReadLine();
+                }
+
+                newEntry._date = DateTime.Now.ToString("yyyy/MM/dd");
+                newEntry._prompt = currentPrompt;
+                newEntry._entryText = inputEntry;
+
+                myJournal.AddEntry(newEntry);
+
+                newEntry = new Entry();
+            }
+            else if (choice == 2)
+            {
+                if (myJournal._entries != null)
+                {
+                    myJournal.DisplayAll();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the current journal is empty.");
+                }
+            }
+            else if (choice == 3)
+            {
+                if (myJournal._entries != null)
+                {
+                    Console.WriteLine("What is the filename? ");
+                    string FileName = Console.ReadLine();
+
+                    while (!string.IsNullOrWhiteSpace(FileName))
+                    {
+                        Console.Write("Please enter a valid file name: ");
+                        FileName = Console.ReadLine();
+                    }
+
+                    myJournal.SaveToFile(FileName);
+                }
+                else
+                {
+                    Console.WriteLine("There is nothing to save.");
+                }
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("What is the filename? ");
+                string FileName = Console.ReadLine();
+
+                while (!string.IsNullOrWhiteSpace(FileName))
+                {
+                    Console.Write("Please enter a valid file name: ");
+                    FileName = Console.ReadLine();
+                }
+
+                myJournal.LoadFromFile(FileName);
+
+            }
+            else if (choice == 5)
+            {
+                break;
+            }
+        }
     }
 }
